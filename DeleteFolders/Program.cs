caa1f3b33f852736fd1f9b9029743d0c5642da88
cjoycejha <cjoyce@jackhenry.com>
2023-11-21 15:53:41 -0600

    Add project files.

---
+++ DeleteFolders/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Hello, World!");
+var searchString = "obj";
+var entries = Directory.GetFileSystemEntries(@"F:\Git", "*", SearchOption.AllDirectories).ToList();
+var dirs = entries.Where(str => str.EndsWith(searchString)).ToList();
+Console.WriteLine(dirs.Count);
+var i = 0;
+foreach(var dir in dirs)
+{
+	Console.WriteLine($"{i++} {dir}");
+	var info = new DirectoryInfo(dir);
+	var count = Regex.Matches(dir, searchString).Count;
+	if (count == 1 && !dir.Contains("DeleteFolders") && info.Exists)
+	{
+		info.Delete(true);
+	}
+}
